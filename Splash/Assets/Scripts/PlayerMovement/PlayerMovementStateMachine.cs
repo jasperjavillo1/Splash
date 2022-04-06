70daec8b15f71bafbde7ef19bd41deb6cf131576
Jasper Javillo <jasperjavillo@gmail.com>
2022-04-06 13:16:50 -0400

    Added PlayerMovement folder and several empty C# Scripts for the various Player Movement States.

---
+++ Splash/Assets/Scripts/PlayerMovement/PlayerMovementStateMachine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Context of the Player Movement State Machine
+public class PlayerMovementStateMachine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
