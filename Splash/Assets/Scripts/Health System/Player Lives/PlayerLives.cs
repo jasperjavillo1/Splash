bd31b363ecb7ed8dcb944104718330b667aa5fb8
EricOcampo <OcampoEric1694@gmail.com>
2022-04-07 13:02:21 -0700

    Credits, Lives, and Game Over have been added
    Player Prefab has been update to  hold the Lives System as well as the Game Over and Credits

---
+++ Splash/Assets/Scripts/Health System/Player Lives/PlayerLives.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerLives : MonoBehaviour
+{
+    public float _CurrentLives;
+    public GameObject gameOver;
+    public UnityEngine.UI.Text LivesCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LivesCount = GameObject.Find("LivesCountText").GetComponent<UnityEngine.UI.Text>();
+        gameOver.SetActive(false);
+        
+    }
+    private void Update()
+    {
+        LivesCount.text = _CurrentLives.ToString();
+    }
+    public void LoseLives()
+    {
+        _CurrentLives--;
+
+        //Game Over "if" statement.
+        if (_CurrentLives > 1 && _CurrentLives == 0)
+        {
+            gameOver.SetActive(true);
+        }
+    }
+   
+}
---
+++ Splash/Assets/Scripts/Health System/Player Lives/RunCredits.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunCredits : MonoBehaviour
+{
+    public GameObject Credits;
+    private void Start()
+    {
+        Credits.SetActive(false);
+    }
+    private void CreditsOn()
+    {
+        Credits.SetActive(true);
+    }
+}
