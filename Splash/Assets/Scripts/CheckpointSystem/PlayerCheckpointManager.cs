780a6fe3551992add5a56e3b37836883e70708d4
MohsenMajboor <neilmajboor@gmail.com>
2022-04-08 22:44:23 -0400

    Added Checkpoint System
    To add a checkpoint, drag in a checkpoint prefab to the scene, and duplicate it to make more. Make sure each checkpoint has the number when it would occur in the scene (1 would occur 1st, 2 would occur 2nd, etc). Drag all these checkpoints onto the Player's Checkpoint manager script, in order, so they work properly). You can look in the MohsenTestingScene Scene for an example.

---
+++ Splash/Assets/Scripts/CheckpointSystem/PlayerCheckpointManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerCheckpointManager : MonoBehaviour
+{
+    //parameters
+    [SerializeField] List<GameObject> _checkpoints;
+
+    void OnEnable()
+    {
+        transform.position = _checkpoints[PlayerPrefs.GetInt("Current Checkpoint") - 1].transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "PlayerCheckpoint")
+        {
+            int playerCheckpointNumber = Convert.ToInt32(new String(other.name.Where(Char.IsDigit).ToArray()));
+            PlayerPrefs.SetInt("Current Checkpoint", playerCheckpointNumber);
+        }    
+    }
+}
