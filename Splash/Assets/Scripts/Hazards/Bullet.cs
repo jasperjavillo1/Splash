ee3aa1d1680531bbe67a2bf335882fa031c18d7f
MohsenMajboor <neilmajboor@gmail.com>
2022-04-16 13:51:16 -0400

    Made Hazard Bullets Trigger
    Made Hazard Bullets Trigger so player is not pushed backward when in air.

--- Splash/Assets/Scripts/Hazards/Bullet.cs
+++ Splash/Assets/Scripts/Hazards/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour {
     bool moving = false;
     Hazard parent;
 
-    private void OnCollisionEnter2D(Collision2D col) {
+    private void OnTriggerEnter2D(Collider2D col) {
         if(col.gameObject.tag == "Player") {
             parent.dealDamageToPlayer();
         }
