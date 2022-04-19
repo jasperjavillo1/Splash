7c34f8983ef4f9427c301ec4ba1c4772694915d6
ericryanlane <ericryanlane@gmail.com>
2022-04-19 16:13:33 -0500

    Completed Goomba-Like Enemy
    Simple Movement, set xSpeed and xDistance after dropping in scene

--- Splash/Assets/Scripts/Enemies/SimpleEnemyMovement.cs
+++ Splash/Assets/Scripts/Enemies/SimpleEnemyMovement.cs
@@ -4,33 +4,31 @@ using UnityEngine;
 
 public class SimpleEnemyMovement : MonoBehaviour
 {
-    public float xSpeed, ySpeed;
-    public float xEnd, xDirection, xStart;
-    private float xDistance;
+    public float xSpeed, xDistance;
+    private float xDist, xMin, xMax, xDirection;
 
     private void Start()
     {
-        xStart = gameObject.transform.position.x;
-
+        xMin = gameObject.transform.position.x;
+        xMax = xMin + xDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         // Sets enemy movement speed
-        gameObject.transform.position = new Vector2(transform.position.x + (xSpeed * xDirection), transform.position.y);
+        gameObject.transform.position = new Vector2(transform.position.x + (xSpeed * xDirection / 100), transform.position.y);
 
         // Controls direction of movement
-        xDistance = xStart + gameObject.transform.position.x;
+        xDist = gameObject.transform.position.x;
 
         // Move Right
-        if (xDistance <= xStart)
+        if (xDist <= xMin)
         {
             xDirection = .1f;
         }
         // Move Left
-        if (xDistance >= xEnd)
+        if (xDist >= xMax)
         {
             xDirection = -.1f;
         }
