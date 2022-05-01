58fa951f794f35799b9a64b0cec92d4dfffeab86
MohsenMajboor <neilmajboor@gmail.com>
2022-05-01 00:55:18 -0400

    Gun Speed Constant, Health Pickup
    Gun speed is actually constant now.
    Shooting costs health.
    Boomerang Bro has adjustable random probability of dropping health pickup.

---
+++ Splash/Assets/HealthPickup.cs
@@ -0,0 +1,21 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip regenSound;
+    [SerializeField] float healthIncreaseAmount = 500;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            FindObjectOfType<AudioManager>().PlaySound(regenSound);
+            PlayerHealth PH = col.gameObject.GetComponent<PlayerHealth>();
+            PH.IncreaseHealth(healthIncreaseAmount);
+            Destroy(gameObject);
+        }
+    }
+}
